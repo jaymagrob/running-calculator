066d0bd7ab3ed88bd7a422822fc883d4a46f89b3
John Robertson <johnmagnusrobertson@gmail.com>
2023-02-02 17:55:16 +0000

    Run-13: header: add MUI to header

--- components/header.js
+++ components/header.js
@@ -1,24 +1,46 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 export default function Header() {
   return (
-    <header>
-      <h1>
-        <Link href="/">Runners Calculator</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/pace/split-pace">Split pace</Link>
-          </li>
-          <li>
-            <Link href="/pace/negative-split">Negative split</Link>
-          </li>
-          <li>
-            <Link href="/pace/positive-split">Positive split</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Toolbar sx={{ flexWrap: 'wrap' }}>
+    <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+      <NextLink href="/">Runners Calculator</NextLink>
+    </Typography>
+    <nav>
+      <NextLink href="/pace/split-pace">
+        <Link
+          variant="button"
+          color="text.primary"
+          href="#"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Features
+        </Link>
+      </NextLink>
+      <NextLink href="/pace/negative-split">
+        <Link
+          variant="button"
+          color="text.primary"
+          href="#"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Enterprise
+        </Link>
+      </NextLink>
+      <NextLink href="/pace/positive-split">
+        <Link
+          variant="button"
+          color="text.primary"
+          href="#"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Support
+        </Link>
+      </NextLink>
+    </nav>
+  </Toolbar>
   )
 }
