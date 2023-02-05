e143a0e93a3d30cfb1da439f7cf4f033b711dc8c
John Robertson <johnmagnusrobertson@gmail.com>
2023-02-05 19:34:43 +0000

    Run-13: layout: redesigned with mui

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Box from '@mui/material/Box';
 import Header from './header'
 import Footer from './footer'
 import styles from './layout.module.css';
@@ -8,29 +9,52 @@ export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <Header />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+        }}
+      >
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Learn how to build a personal website using Next.js"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1, width=device-width"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle,
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <Header />
+        <Box sx={{ marginTop: 8 }}>
+          {children}
+        </Box>
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: 'auto',
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Footer />
+        </Box>
+      </Box>
+    </>
   )
 }
