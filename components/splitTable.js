dc71a20273a0dacd158bcb3f21a2c29dce73df24
John Robertson <johnmagnusrobertson@gmail.com>
2023-02-05 19:35:04 +0000

    Run-13: splitTable: redesigned with mui

--- components/splitTable.js
+++ components/splitTable.js
@@ -1,3 +1,11 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import Box from '@mui/material/Box';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 export default function SplitTable(props) {
   const { splits } = props;
 
@@ -6,24 +14,36 @@ export default function SplitTable(props) {
   }
 
   return (
-    <table>
-      <thead>
-
-        <tr>
-          <th>KM</th>
-          <th>Split Time</th>
-          <th>Lap Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {splits.map(split => (
-          <tr key={split.km}>
-            <td>{split.km}</td>
-            <td>{split.split}</td>
-            <td>{split.lapTime}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 2,
+        width: '100%',
+      }}
+    >
+      <Table sx={{ width: '100%', maxWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">KM</TableCell>
+            <TableCell align="right">Split Time</TableCell>
+            <TableCell align="right">Lap Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {splits.map((row) => (
+            <TableRow
+              key={row.km}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.km}</TableCell>
+              <TableCell align="right">{row.split}</TableCell>
+              <TableCell align="right">{row.lapTime}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Box>
   )
 }
