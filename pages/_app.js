8cfa801c5a9c709a0cd38de4f4b6e3995cfe9364
John Robertson <johnmagnusrobertson@gmail.com>
2023-02-13 20:22:45 +0000

    RUN-16, layout: replace GTM script with initialize tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import Layout from '../components/layout'
+import TagManager from "react-gtm-module"
+import { useEffect } from 'react'
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({id: 'GTM-5D3WFXG'})
+  });
+
   return (
     <Layout>
       <Component {...pageProps} />
