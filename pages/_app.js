47675f9f6ce22b31112610b7a8d60563430ec7a6
John Robertson <johnmagnusrobertson@gmail.com>
2023-02-18 17:03:45 +0000

    RUN-45, ad adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
-import Layout from '../components/layout'
-import TagManager from "react-gtm-module"
-import { useEffect } from 'react'
-import '../styles/globals.scss'
+import Script from "next/script";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+import Layout from "../components/layout";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    TagManager.initialize({gtmId: 'GTM-5D3WFXG'})
+    TagManager.initialize({ gtmId: "GTM-5D3WFXG" });
   }, []);
 
   return (
     <Layout>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-6809452858412935"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
