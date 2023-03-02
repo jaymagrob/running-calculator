4e64a80042e28d838977b6da794ec7c031b39679
John Robertson <johnmagnusrobertson@gmail.com>
2023-03-02 19:23:18 +0000

    RUN-44, positive: add popular and miles

--- pages/pace/positive-split.jsx
+++ pages/pace/positive-split.jsx
@@ -5,6 +5,7 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
 import Head from "next/head";
+import FormControl from "@mui/material/FormControl";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -15,12 +16,33 @@ import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { useState } from "react";
 import SplitTable from "../../components/splitTable";
 
+function convertKtM(a) {
+  return a / 1.609344;
+}
+
+function convertMtK(a) {
+  return a * 1.609344;
+}
+
+const popularList = [
+  { value: "0", label: "5km", k: 5, m: convertKtM(5) },
+  { value: "1", label: "10km", k: 10, m: convertKtM(10) },
+  { value: "2", label: "5 miles", k: convertMtK(5), m: 5 },
+  { value: "3", label: "15km", k: 15, m: convertKtM(15) },
+  { value: "4", label: "10 miles", k: convertMtK(10), m: 10 },
+  { value: "5", label: "Half Marathon", k: convertMtK(13.1), m: 13.1 },
+  { value: "6", label: "Marathon", k: convertMtK(26.2), m: 26.2 },
+  { value: "7", label: "50km", k: 50, m: convertKtM(50) },
+];
+
 export default function splitPace() {
-  const [distance, setDistance] = useState("5000");
+  const [distance, setDistance] = useState("5");
   const [time, setTime] = useState(dayjs("2022-01-01 00:20:00"));
-  const [positiveSplits, setpositiveSplits] = useState(0.05);
+  const [positiveSplits, setPositiveSplits] = useState(0.05);
   const [splits, setSplits] = useState([]);
   const [isError, setIsError] = useState(false);
+  const [metric, setMetric] = useState("k");
+  const [popular, setPopular] = useState(null);
 
   function onSubmit(e) {
     e.preventDefault();
@@ -30,7 +52,7 @@ export default function splitPace() {
       return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
     }
 
-    if (parseFloat(distance) > 0 && parseFloat(distance) <= 100000) {
+    if (parseFloat(distance) > 0 && parseFloat(distance) <= 200) {
       setIsError(false);
     } else {
       setIsError(true);
@@ -40,14 +62,14 @@ export default function splitPace() {
     const minute = time.$m;
     const seconds = time.$s;
     const milli = minute * 60 * 1000 + seconds * 1000;
-    const newDistance = distance / 1000;
-    const pace = milli / newDistance;
+    const newDistance = distance * 1000;
+    const pace = milli / distance;
     const pace1 = pace * (1 - positiveSplits);
     const pace2 = pace * (1 + positiveSplits);
-    const halfway = distance / 2;
+    const halfway = newDistance / 2;
     const loops = [];
     let totalSplit = 0;
-    for (let i = 1; i <= Math.ceil(newDistance); i += 1) {
+    for (let i = 1; i <= Math.ceil(distance); i += 1) {
       if ((i - 1) * 1000 <= halfway && i * 1000 >= halfway) {
         const firstPace = ((halfway - (i - 1) * 1000) / 1000) * pace1;
         const secondPace = ((i * 1000 - halfway) / 1000) * pace2;
@@ -67,10 +89,10 @@ export default function splitPace() {
         });
       } else {
         totalSplit +=
-          i > newDistance ? pace2 * ((distance % 1000) / 1000) : pace2;
+          i > distance ? pace2 * ((newDistance % 1000) / 1000) : pace2;
         loops.push({
           km:
-            i > newDistance ? i - 1 + parseFloat((distance % 1000) / 1000) : i,
+            i > distance ? i - 1 + parseFloat((newDistance % 1000) / 1000) : i,
           split: millisToMinutesAndSeconds(totalSplit),
           lapTime: millisToMinutesAndSeconds(pace2),
         });
@@ -95,21 +117,68 @@ export default function splitPace() {
             alignItems: "center",
             gap: 2,
             width: "100%",
+            maxWidth: "200px",
+            margin: "0 auto",
           }}
         >
           <Typography component="h1" variant="h5">
             Positive Split
           </Typography>
-          <TextField
-            type="number"
-            inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-            id="distance"
-            label="Distance"
-            value={distance}
-            onChange={(e) => setDistance(e.target.value)}
-            error={isError}
-            helperText={isError ? "Between 1 and 100,000" : null}
-          />
+          <FormControl fullWidth>
+            <TextField
+              type="number"
+              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              id="distance"
+              label="Distance"
+              value={distance}
+              onChange={(e) => setDistance(e.target.value)}
+              error={isError}
+              helperText={isError ? "Between 0 and 200" : null}
+            />
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="metric-select-label">Metric</InputLabel>
+            <Select
+              labelId="metric-select-label"
+              id="metric-select"
+              value={metric}
+              label="Metric"
+              onChange={(e) => {
+                const metric2 = e.target.value;
+                if (metric2 === "k") {
+                  setDistance(convertMtK(distance));
+                }
+                if (metric2 === "m") {
+                  setDistance(convertKtM(distance));
+                }
+                setMetric(metric2);
+              }}
+            >
+              <MenuItem value="k">Kilometers</MenuItem>
+              <MenuItem value="m">Miles</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="popular-select-label">Popular</InputLabel>
+            <Select
+              labelId="popular-select-label"
+              id="popular-select"
+              value={popular}
+              label="popular"
+              onChange={(e) => {
+                const { value } = e.target;
+                setPopular(value);
+                const data = popularList.find((i) => i.value === value);
+                setDistance(data[metric]);
+              }}
+            >
+              {popularList.map((item) => (
+                <MenuItem key={item.label} value={item.value}>
+                  {item.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <TimePicker
               ampmInClock
@@ -138,7 +207,7 @@ export default function splitPace() {
               id="demo-simple-select"
               value={positiveSplits}
               label="Age"
-              onChange={(e) => setpositiveSplits(e.target.value)}
+              onChange={(e) => setPositiveSplits(e.target.value)}
             >
               <MenuItem value={0.1}>10%</MenuItem>
               <MenuItem value={0.09}>9%</MenuItem>
